bb704d4c3868d0a149ccc6c8f315bb13bf93c130
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-18 14:37:21 +0700

    feat: create form product

--- src/components/dashboard/product/ProductsAdd.tsx
+++ src/components/dashboard/product/ProductsAdd.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React from "react";
+import { MdArrowDropDown } from "react-icons/md";
 
 const ProductAdd = () => {
+  const options = ["Makanan", "Minuman"];
   return (
-    <div>ProductAdd</div>
-  )
-}
+    <div className="flex flex-col gap-9">
+      <div className="rounded-sm border border-stroke text-white bg-gray-900 shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="border-b border-stroke px-6 py-4 dark:border-strokedark">
+          <h3 className="font-medium">Tambah Products</h3>
+        </div>
+        <form action="#">
+          <div className="p-6">
+            <div className="mb-4">
+              <label className="mb-3 block text-sm font-medium">
+                Nama Produk
+              </label>
+              <input
+                type="text"
+                placeholder="Masukan Nama Product"
+                className="w-full rounded border-[1px] border-stroke bg-transparent px-3 py-2 font-normal outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                name="name"
+              />
+            </div>
 
-export default ProductAdd
+            <div className="mb-4">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                Kategori
+              </label>
+              <div className="relative z-20 bg-transparent dark:bg-form-input">
+                <select
+                  name="categoryId"
+                  className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-3 py-2 outline-none transition focus:border-primary active:border-primary dark:focus:border-primary"
+                >
+                  <option className="text-black">Pilih Kategori</option>
+                  {options.map((option, index) => (
+                    <option key={index} value={option} className="text-black">
+                      {option}
+                    </option>
+                  ))}
+                </select>
+                <span className="absolute right-4 top-1/2 z-30 -translate-y-1/2">
+                  <MdArrowDropDown className="fill-current" size={24} />
+                </span>
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <label className="mb-3 block text-sm font-medium">
+                Stok Produk
+              </label>
+              <input
+                type="text"
+                placeholder="Masukan Berapa Stok Produk"
+                className="w-full rounded border-[1px] border-stroke bg-transparent px-3 py-2 font-normal outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                name="stock"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="mb-3 block text-sm font-medium">
+                Harga Produk
+              </label>
+              <input
+                type="text"
+                placeholder="Masukan Harga Produk"
+                className="w-full rounded border-[1px] border-stroke bg-transparent px-3 py-2 font-normal outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+              />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
+              <div className="mx-auto">
+                <img
+                  src={""}
+                  alt="Buku Yang Mau di Upload"
+                  className="max-w-[200px] h-[200px] max-h-[200px] mx-auto"
+                />
+              </div>
+              <div>
+                <label className="mb-3 block text-sm font-medium">
+                  Harga Produk
+                </label>
+                <input
+                  type="file"
+                  name="image"
+                  placeholder="Masukkan URL Gambar Buku"
+                  className="w-full rounded border-[1px] border-stroke bg-transparent px-3 py-2 font-normal outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter"
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ProductAdd;
