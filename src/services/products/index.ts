3f66a16faa91c00d74c55eefe7dfdf8a96da0e6c
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-20 00:02:35 +0700

    feat: create update and delete products

--- src/services/products/index.ts
+++ src/services/products/index.ts
@@ -6,4 +6,5 @@ import { ProductModel } from "@model/product";
 export const productService = {
     get: HTTP_REQUEST.get<ApiResponse<ProductModel[]>>(API_ENDPOINT.products),
     post: HTTP_REQUEST.post<ApiResponse<ProductModel>>(API_ENDPOINT.products),
+    put: HTTP_REQUEST.put<ApiResponse<ProductModel>>(API_ENDPOINT.products),
 }
