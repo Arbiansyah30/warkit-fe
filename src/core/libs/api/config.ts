4be919ac7ebaa0e4239243d96c356ad50287b204
muhammadfikri4 <unicornhorse1@gmail.com>
2024-06-27 13:32:53 +0700

    fix: delete stringify

--- src/core/libs/api/config.ts
+++ src/core/libs/api/config.ts
@@ -40,7 +40,7 @@ async function createRequest<Res = unknown, Req = unknown>(
         }),
         ...apiOption?.headers,
       },
-      data: JSON.stringify(body),
+      data: body,
 
     }
   );
