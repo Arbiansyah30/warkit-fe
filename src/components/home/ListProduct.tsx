53b039d0d1960bc15bac5982f04886a6815fbc29
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-07-16 22:21:34 +0700

    new update ui card product

--- src/components/home/ListProduct.tsx
+++ src/components/home/ListProduct.tsx
@@ -38,7 +38,7 @@ const ListProduct = () => {
             />
           </div>
         </div>
-        <div className="grid py-3 max-sm:grid-cols-2 max-lg:grid-cols-3 max-2xl:grid-cols-4 grid-cols-5 max-sm:gap-3 gap-5 items-center justify-center">
+        <div className="grid py-3 max-sm:grid-cols-2 max-lg:grid-cols-3 max-2xl:grid-cols-4 grid-cols-5 max-sm:gap-3 gap-5 items-start justify-center">
           {products?.data?.map((item) => (
             <CardProduct
               key={item.id}
