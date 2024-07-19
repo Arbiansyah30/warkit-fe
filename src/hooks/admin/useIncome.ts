f614fb02f52722c5cb1222a6046f14c56eb0f711
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-07-19 20:32:35 +0700

    fix: pagination

--- src/hooks/admin/useIncome.ts
+++ src/hooks/admin/useIncome.ts
@@ -5,13 +5,18 @@ import { incomeService } from "../../services/income";
 export function useIncome() {
     const [searchParams] = useSearchParams();
     const date = searchParams.get("date") || undefined;
+    const page = searchParams.get("page") || 1;
+    const perPage = searchParams.get("perPage") || 10;
+
     // const from = searchParams.get("from") || undefined;
     // const to = searchParams.get("to") || undefined;
     const query = useQuery({
-        queryKey: ['income', { date }],
+        queryKey: ['income', { date, page, perPage }],
         queryFn: () => incomeService.get({
             queryParams: {
-                date
+                date,
+                page: page ? Number(page) : undefined,
+                perPage: perPage ? Number(perPage) : undefined
             }
         })
     })
